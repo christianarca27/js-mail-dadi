a4afe22913f7c26d0c3a5923c1e61de6413ba7f6
christianarca27 <christian.arca.mail@gmail.com>
2023-02-27 13:55:46 +0100

    refactor: aggiunti commenti script

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
+// Array contenente gli indirizzi email validi per giocare
 const validEmail = ["christian@email.com", "matteo@email.com", "marco@email.com"];
 
+// Leggo e salvo il riferimento al pulsante di inizio gioco
 let buttonLoginEl = document.getElementById("btn-login");
 
+// Aggiungo un evento in caso il pulsante di inizio gioco venga cliccato
 buttonLoginEl.addEventListener("click", function() {
+    // Leggo e salvo il valore inserito nel campo email al momento del click
     emailInput = document.getElementById("email-input").value;
 
+    // - Inizializzo accessoConsentito a false
+    // - Confronto uno ad uno gli elementi dell'array per controllare se è presente l'email inserita dall'utente
+    // - In caso di match cambio il valore di accessoConsentito a true
     let accessoConsentito = false;
     for(let i = 0; i < validEmail.length; i++) {
         if(emailInput == validEmail[i]) {
@@ -12,6 +19,13 @@ buttonLoginEl.addEventListener("click", function() {
         }
     }
 
+    // - Se il valore di accessoConsentito è stato cambiato in true durante il ciclo:
+    //      # Estraggo un numero random per il giocatore
+    //      # Estraggo un numero random per il computer
+    //      # Confronto i due valori e in base al risultato decido l'esito della partita
+    // 
+    // - Altrimenti:
+    //      # Comunico che l'accesso è fallito
     if(accessoConsentito) {
         console.log("Giochiamo !");
 
