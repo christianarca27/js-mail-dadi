4173cdde4c9e884f30f0c0a57337120322eb4730
christianarca27 <christian.arca.mail@gmail.com>
2023-02-27 15:12:47 +0100

    feat: aggiunte icone dadi e stile pagina

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // Array contenente gli indirizzi email validi per giocare
 const validEmail = ["christian@email.com", "matteo@email.com", "marco@email.com"];
+const diceFaces = ["fa-dice-one", "fa-dice-two", "fa-dice-three", "fa-dice-four", "fa-dice-five", "fa-dice-six"];
 
 // Leggo e salvo il riferimento al pulsante di inizio gioco
 let buttonLoginEl = document.getElementById("btn-login");
@@ -20,7 +21,11 @@ buttonLoginEl.addEventListener("click", function() {
         }
     }
 
-    let gameEl = document.getElementById("game-table");
+    let messageEl = document.getElementById("message");
+
+    let gameTableEl = document.getElementById("game-table");
+
+    let resultEl = document.getElementById("result");
 
     // - Se il valore di accessoConsentito è stato cambiato in true durante il ciclo:
     //      # Estraggo un numero random per il giocatore
@@ -30,32 +35,42 @@ buttonLoginEl.addEventListener("click", function() {
     // - Altrimenti:
     //      # Comunico che l'accesso è fallito
     if(accessoConsentito) {
-        gameEl.style.color = "black";
-        gameEl.innerHTML = "Accesso consentito, giochiamo.. <br>";
+        messageEl.style.color = "black";
+        messageEl.innerText = "Accesso consentito, giochiamo..";
+
+        let playerDiceEl = document.getElementById("player-dice");
+        playerDiceEl.className = "fa-solid fa-10x";
+        let computerDiceEl = document.getElementById("computer-dice");
+        computerDiceEl.className = "fa-solid fa-10x";
 
         let lancioGiocatore = Math.ceil(Math.random() * 6);
+        console.log("Dado giocatore: " + lancioGiocatore);
+        playerDiceEl.classList.add(diceFaces[lancioGiocatore - 1]);
+
 
         let lancioComputer = Math.ceil(Math.random() * 6);
+        console.log("Dado computer: " + lancioComputer);
+        computerDiceEl.classList.add(diceFaces[lancioComputer - 1]);
 
         if(lancioGiocatore > lancioComputer) {
-            gameEl.innerHTML += "Hai vinto !<br>";
-            gameEl.innerHTML += "Dado giocatore : " + lancioGiocatore + "<br>";
-            gameEl.innerHTML += "Dado computer : " + lancioComputer;
+            resultEl.innerText = "Hai Vinto !"
         }
         else if(lancioGiocatore == lancioComputer) {
-            gameEl.innerHTML += "Pareggio !<br>";
-            gameEl.innerHTML += "Dado giocatore : " + lancioGiocatore + "<br>";
-            gameEl.innerHTML += "Dado computer : " + lancioComputer;
+            resultEl.innerText = "Pareggio"
         }
         else {
-            gameEl.innerHTML += "Hai perso !<br>";
-            gameEl.innerHTML += "Dado giocatore : " + lancioGiocatore + "<br>";
-            gameEl.innerHTML += "Dado computer : " + lancioComputer;
+            resultEl.innerText = "Hai Perso !"
         }
+
+        gameTableEl.style.display = "flex";
+        resultEl.style.display = "flex";
     }
     else {
-        gameEl.style.color = "red";
-        gameEl.innerHTML = "Accesso negato, email non valida.";
+        gameTableEl.style.display = "none";
+        resultEl.style.display = "none";
+        
+        messageEl.style.color = "red";
+        messageEl.innerText = "Accesso non consentito, inserisci un indirizzo email valido.";
     }
     // Svuoto il campo di email per poter iniziare una nuova partita
     emailInputEl.value = "";
